ba87e18944d5c67fbf3ca6a79b42b9e20bbbb966
IVika-QA <apple@MacBook-Pro-Apple.local>
2024-07-28 12:42:44 +0300

    add Project1 from Lesson

---
+++ Project1/src/test/java/HelloFromVikaITest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloFromVikaITest {
+
+    @Test
+    public void testHelloFromUser() {
+            System.out.println("Hello from Vika Ibraimova");
+
+    }
+}
